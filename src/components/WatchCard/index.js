71b02ae2fcbfd388c64295d8876e031f0dce77ef
Alina Kovaleva <calipco.xp@gmail.com>
2022-12-08 01:01:31 +0100

    small fixes for rating display

--- src/components/WatchCard/index.js
+++ src/components/WatchCard/index.js
@@ -10,39 +10,45 @@ const textCrop = (str, n) => {
 
 function WatchCard(props) {
   return (
-    <div className="card-wrap">
-      <img className="card-img" alt={props.title} src={props.image} />
-      <h4>{props.title}</h4>
-      <div className="mb-5">
-        <Currency quantity={props.price} currency="EUR" />
+    <div>
+      <div className="card-wrap">
+        <img className="card-img" alt={props.title} src={props.image} />
+        <h4>{props.title}</h4>
+        <div className="mb-5">
+          <Currency quantity={props.price} currency="EUR" />
+        </div>
+        <div className="rating-wrap">
+          <div className="rating">
+            <ReactStars value={props.rating} size={25} color2={'#ffd700'} />
+            <p>{props.rating}</p>
+            <p>({props.reviewCount})</p>
+          </div>
+          <div className="stock">
+            {props.stock === 'inStock' ? (
+              <p>✔️ In stock</p>
+            ) : (
+              <p>Out of stock</p>
+            )}
+          </div>
+        </div>
+        <div className="card-body">
+          <div>
+            <ul>
+              {props.storePromotions.map((item) => {
+                return <li>{textCrop(item.promotionText, 40)}</li>;
+              })}
+            </ul>
+          </div>
+          <div>
+            <ul>
+              {props.localBenefitList.map((item) => {
+                return <li>{item.localBenefitText}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+        <button className="card-btn">Add to Basket</button>
       </div>
-      <div className="rating-wrap">
-        <div className="rating">
-          <ReactStars value={props.rating} size={25} color2={'#ffd700'} />
-          <p>{props.rating}</p>
-          <p>({props.reviewCount})</p>
-        </div>
-        <div className="stock">
-          {props.stock === 'inStock' ? <p>✔️ In stock</p> : <p>Out of stock</p>}
-        </div>
-      </div>
-      <div className="card-body">
-        <div>
-          <ul>
-            {props.storePromotions.map((item) => {
-              return <li>{textCrop(item.promotionText, 40)}</li>;
-            })}
-          </ul>
-        </div>
-        <div>
-          <ul>
-            {props.localBenefitList.map((item) => {
-              return <li>{item.localBenefitText}</li>;
-            })}
-          </ul>
-        </div>
-      </div>
-      <button className="card-btn">Add to Basket</button>
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,6 +4,10 @@ import Axios from 'axios';
 
 import { Header, WatchCard, MobileCard, TabletCard } from '../../components';
 
+function financial(x) {
+  return Number.parseFloat(x).toFixed(1);
+}
+
 export default function Home() {
   const API_URL =
     'https://searchapi.samsung.com/v6/front/b2c/product/card/detail/newhybris?siteCode=nl&modelList=SM-F721BZAGEUB,SM-F721BZAHEUB,SM-F721BZAPEUB,SM-F721BZDGEUB,SM-F721BZDHEUB,SM-F721BZDPEUB,SM-F721BLBGEUB,SM-F721BLBHEUB,SM-F721BLBPEUB,SM-F721BLVGEUB,SM-F721BLVHEUB,SM-F721BLVPEUB,SM-F936BZABEUB,SM-F936BZACEUB,SM-F936BZANEUB,SM-F936BZEBEUB,SM-F936BZECEUB,SM-F936BZENEUB,SM-F936BZKBEUB,SM-F936BZKCEUB,SM-F936BZKNEUB,SM-F936BDRBEUB,SM-F936BDRCEUB,SM-S906BIDDEUB,SM-S906BZGDEUB,SM-S906BZKDEUB,SM-S906BZWDEUB,SM-S906BIDGEUB,SM-S906BZGGEUB,SM-S906BZKGEUB,SM-S906BZWGEUB,SM-S906BZADEUB,SM-S906BZEDEUB,SM-S906BZVDEUB,SM-S906BLBDEUB,SM-S906BZAGEUB,SM-S906BZEGEUB,SM-S906BZVGEUB,SM-S906BLBGEUB,SM-R920NZKDWEU,SM-R920NZTDWEU,SM-R900NZAAEUB,SM-R900NZSAEUB,SM-R900NZDAEUB,SM-X700NIDAEUB,SM-X700NZAAEUB,SM-X700NZABEUB,SM-X700NZSAEUB,SM-X700NZSBEUB,SM-X706BZAAEUB,SM-X706BZABEUB,QE65QN95BATXXN,QE75QN95BATXXN,QE85QN95BATXXN,QE65QN800BTXXN,QE75QN800BTXXN,QE85QN800BTXXN,QE65QN900BTXXN,HW-Q990B/XN,HW-Q930B/XN,HW-Q700B/XN&commonCodeYN=N&saleSkuYN=N&onlyRequestSkuYN=Y&keySummaryYN=N&shopSiteCode=nl';
@@ -26,7 +30,7 @@ export default function Home() {
           : data.map((item) => {
               if (item.categorySubTypeEngName === 'Galaxy Watch') {
                 return (
-                  <>
+                  <div>
                     {item.modelList.map((model) => {
                       return (
                         <WatchCard
@@ -35,21 +39,21 @@ export default function Home() {
                           image={model.galleryImage[0]}
                           localBenefitList={item.localBenefitList}
                           stock={model.ctaType}
-                          rating={model.ratings}
+                          rating={financial(model.ratings)}
                           price={model.price}
                           storePromotions={model.storePromotions}
                           reviewCount={model.reviewCount}
                         />
                       );
                     })}
-                  </>
+                  </div>
                 );
               } else if (
                 item.categorySubTypeEngName === 'Galaxy Z' ||
                 item.categorySubTypeEngName === 'Galaxy S'
               ) {
                 return (
-                  <>
+                  <div>
                     {item.modelList.map((model) => {
                       return (
                         <MobileCard
@@ -57,7 +61,7 @@ export default function Home() {
                           title={model.displayName}
                           stock={model.ctaType}
                           price={model.price}
-                          rating={model.ratings}
+                          rating={financial(model.ratings)}
                           reviewCount={model.reviewCount}
                           storePromotions={model.storePromotions}
                           localBenefitList={item.localBenefitList}
@@ -65,7 +69,7 @@ export default function Home() {
                         />
                       );
                     })}
-                  </>
+                  </div>
                 );
               } else if (item.categorySubTypeEngName === 'Galaxy Tab S') {
                 return (
@@ -80,7 +84,7 @@ export default function Home() {
                           storePromotions={model.storePromotions}
                           price={model.price}
                           stock={model.ctaType}
-                          rating={model.ratings}
+                          rating={financial(model.ratings)}
                           reviewCount={model.reviewCount}
                         />
                       );
